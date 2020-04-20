fb77dc9b3a5bbc36968675208ca061e2340e07bd
samuel <samuel@pop-os.localdomain>
2020-04-19 22:05:07 -0500

    samuel commit

--- vueapp/src/router.js
+++ vueapp/src/router.js
@@ -3,6 +3,8 @@ import Login from './pages/Login/Login'
 import Register from './pages/Register/Register'
 import Admin from './pages/Admin/Admin'
 import UserManagement from "./pages/Admin/views/UserManagement"
+import GeneralForum from "./pages/User/view/GeneralForum"
+import User from './pages/User/User'
 
 export default new Router({
     routes: [
@@ -31,6 +33,19 @@ export default new Router({
               }
           ]
       },
+      {
+        path: '/userPane',
+        component: User,
+        name: "User",
+        redirect: "/userPane/generalForum",
+        children:[
+          {
+            name:"generalForum",
+            path:"generalForum",
+            component: GeneralForum,
+          }
+        ]
+      }
       
       
     ]
